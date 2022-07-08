3163829fbbad872a773867300eecaac1d76503d7
betim <betimbytyqi86@gmaill.com>
2022-07-08 19:24:33 +0200

    only the search left

--- src/Body.js
+++ src/Body.js
@@ -7,24 +7,33 @@ import Card from './Card'
 function Body() {
 
     const [info,setInfo]= useState();
-
+    const [loadin,setLoading]=useState(true);
+    const [num,setNum] = useState(12);
     const data = async() => {
-        let num = 10;
-        const fetching = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${num}`);
+      
+        const url =`https://pokeapi.co/api/v2/pokemon?limit=${num}`;
+        const fetching = await fetch(url);
         const json = await fetching.json();
         const results = await json.results; 
-        setInfo(results);
+        setInfo(info=>info=results);
     }
 
-    useEffect(()=>{      
-       data()
-       console.log(info)
+    useEffect(()=>{  
+      setLoading(false);    
+       data();
+      setLoading(true);
     },[])
   
   return (
-    <div className='Body'>
-      <Card/>
-    </div>
+    <>
+      <div className='Body'>
+          {info?info.map((item)=>{
+              return <Card name={item.name} key={item.name}  pokeUrl={item.url} />
+            }): <p>is loading</p>
+          }
+      
+    </div><button onClick={()=>{setNum(num+12);data();}}>more cards</button>
+    </>     
   )
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
-function Card() {
+function Card({name,pokeUrl}) {
+  const [sprites,setSprites]=useState("");
+  const [types,setTypes]=useState();
+  const [loaded, setLoaded]=useState(false)
+
+  const getCardInfo = async ()=>{
+    const cardInfo= await fetch(pokeUrl);
+    const response= await cardInfo.json();
+    const sprite = await response.sprites.front_default;
+    const type = await response.types;
+    setSprites(sprite);
+    setTypes(type);
+    setLoaded(true)
+  }
+  
+  useEffect(()=>{
+    getCardInfo();
+  },[])
+
     return ( 
       <div className='Card'> 
-        <h3>betim</h3>
-       
+        <h2>{name}</h2>
+       <img src={sprites} alt="" />
+       <div className='types'>
+          {
+            loaded?types.map(type=>{
+              return <div className='type' key={type.type.name} >{type.type.name}</div>
+          }):<p>is loading</p>}
+       </div>
 
       </div>
     )}
